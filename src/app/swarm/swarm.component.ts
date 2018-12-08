06b948ae8073edc923f08d5d479845a3849674d7
pmarecki <pmarecki.pro@gmail.com>
2018-12-08 21:55:23 +0100

    table

--- src/app/swarm/swarm.component.ts
+++ src/app/swarm/swarm.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Device} from '../model/device';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-swarm',
@@ -10,6 +10,8 @@ import {HttpClient} from '@angular/common/http';
 export class SwarmComponent implements OnInit {
   devices: Device[];
   mainUrl = 'http://10.10.0.55:7777';
+  warn= false;
+  newMAC: string;
 
   constructor(private http: HttpClient) { }
 
@@ -24,6 +26,26 @@ export class SwarmComponent implements OnInit {
       });
   }
 
+  // @GetMapping(value = "/identify")
+  // public Device identify(
+  //   @RequestParam(value = "mac") String mac,
+  // @RequestParam(value = "name", required = false) String name,
+  // @RequestParam(value = "nameA", required = false) String nameA,
+  // @RequestParam(value = "nameB", required = false) String nameB,
+  // @RequestParam(value = "nameC", required = false) String nameC
 
 
+  saveDevice(d: Device) {
+    const url = this.mainUrl + '/identify';
+    const params = new HttpParams()
+      .set('mac', d.macAddress)
+      .set('name', d.name)
+      .set('nameA', d.nameA)
+      .set('nameB', d.nameB)
+      .set('nameC', d.nameC);
+    this.http.get<Device>(url,{params})
+      .subscribe(value => {
+        d = value;
+      });
+  }
 }
