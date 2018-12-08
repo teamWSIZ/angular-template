2b22a7cae53c41dd519e4ffab3cf73b73eb9c31b
pmarecki <pmarecki.pro@gmail.com>
2018-12-08 15:35:17 +0100

    add swarm--

--- src/app/swarm/swarm.component.ts
+++ src/app/swarm/swarm.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Device} from '../model/device';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-swarm',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swarm.component.css']
 })
 export class SwarmComponent implements OnInit {
+  devices: Device[];
+  mainUrl = 'http://10.10.0.55:7777';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    console.log('ściągam listę deviceów');
+    //sposób na ściąganie danych po http
+    const url = this.mainUrl + '/devices';
+    this.http.get<Device[]>(url)
+      .subscribe(value => {
+        this.devices = value;
+        console.log('Devices:' + JSON.stringify(this.devices));
+      });
   }
+
+
 
 }
